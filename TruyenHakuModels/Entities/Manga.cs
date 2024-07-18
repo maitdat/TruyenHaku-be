776243233e4c30bf67519f416dadb15c3fc3a16d
Đạt Mai Tiến <maitd2010@gmail.com>
2024-07-18 21:04:41 +0700

    format-name-document

---
+++ TruyenHakuModels/Entities/Manga.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCommon;
+
+namespace TruyenHakuModels.Entities
+{
+    public class Manga : BaseEntity
+    {
+        
+    }
+}
